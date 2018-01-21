d383c114ba4b1a869d1a93e2865d8209ad9fc0bd
Artem Buslov <buslov.artem@gmail.com>
2018-01-21 17:56:42 +0300

    Added Russian messages

---
+++ messages/ru/base.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        '<strong>Pinterest</strong>' => '<strong>Pinterest</strong>',
+        'Pinterest Settings' => 'Настройки Pinterest',
+        'Pinterest URL:' => 'Pinterest URL:',
+        '<strong>Pinterest</strong> module configuration' => 'Конфигурация модуля <strong>Pinterest</strong>',
+        'Save' => 'Сохранить',
+    ];
