2d10ac8082231e27a769e77265fdd76ed70ae8bd
Blavkm <blavkm@gmail.com>
2019-05-27 21:20:27 +0200

    Create base.php

---
+++ messages/es-ES/base.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        '<strong>Pinterest</strong>' => '<strong>Pinterest</strong>',
+        'Pinterest Settings' => 'Ajustes de Pinterest',
+        'Pinterest URL:' => 'URL de Pinterest:',
+        '<strong>Pinterest</strong> module configuration' => 'Configuración de módulo <strong>Pinterest</strong>',
+        'Save' => 'Guardar',
+    ];
